c0ea8b53ef1ec217cafa38c1af840c3516461135
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2021-03-16 15:20:10 +0200

    4. uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,16 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(e){
+    const button = document.querySelector("#button");
+    button.addEventListener("click", function(e){
+
+        const togglableElements = document.getElementsByClassName("toggle_me");
+        Array.from(togglableElements).forEach(element => {
+            if(element.style.display == "none"){
+                element.style.display = "block";
+            }
+            else{
+                element.style.display = "none";
+            }
+        })
+    })
+})
