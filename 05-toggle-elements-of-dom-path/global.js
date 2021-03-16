96244b3129bca2d0b3a1b2b5db69ea8de7b1c890
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2021-03-16 15:32:08 +0200

    5. uzdevums

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(e){
+    const button = document.querySelector("#toggle_button");
+    button.addEventListener("click", function(e){
+        const list = document.querySelectorAll(".second_five li");
+        list.forEach(element => {
+            if(element.style.display == "none"){
+                element.style.display = "list-item";
+            }
+            else{
+                element.style.display = "none";
+            }
+        });
+    })
+})
