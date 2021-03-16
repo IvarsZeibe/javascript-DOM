fba9f8e6e4542a0e0db0e7a8b37c8736b667de62
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2021-03-16 15:42:38 +0200

    6. uzdevums

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,9 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(e){
+    const textbox = document.querySelector("#full_name");
+    textbox.addEventListener("input", function(e){
+        const greeting = document.querySelector("#greeting");
+        greeting.innerHTML = "Sveiks, " + textbox.value + "!";
+        console.log(textbox.textContent);
+    })
+})
